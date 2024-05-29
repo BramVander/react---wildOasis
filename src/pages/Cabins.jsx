4fdd7671349729802b1c5914e132a9653d297b65
BramVander <bramvander@outlook.com>
2024-05-29 16:36:47 +0200

    .

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
+import CabinTable from "../features/cabins/CabinTable";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 import Heading from "../ui/Heading";
+import Button from "../ui/Button";
 import Row from "../ui/Row";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+
+        <Button onClick={() => setShowForm((show) => !show)}>
+          {!showForm ? "Add new cabin" : "Hide form"}
+        </Button>
+        {showForm && <CreateCabinForm></CreateCabinForm>}
+      </Row>
+    </>
   );
 }
 
---
+++ src/services/apiCabins.js
@@ -0,0 +1,37 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  const { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins couldn't be loaded");
+  }
+
+  return data;
+}
+
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
+    .select();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins couldn't be created");
+  }
+
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin couldn't be deleted");
+  }
+
+  return data;
+}
