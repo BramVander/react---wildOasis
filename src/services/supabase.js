bdc8092394a98e5aee1af5fdec4bb0ad35182fcd
BramVander <bramvander@outlook.com>
2024-09-17 15:42:14 +0200

    create cabin form
    - formrow component
    - image upload

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://wecztqpltemmedjzqaho.supabase.co";
+export const supabaseUrl = "https://wecztqpltemmedjzqaho.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndlY3p0cXBsdGVtbWVkanpxYWhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY5NzEyNzgsImV4cCI6MjAzMjU0NzI3OH0.ZzE7fZIvt2tgvFvy0ER-0xSSS7L-V27BZ59CWipXL8s";
 const supabase = createClient(supabaseUrl, supabaseKey);
